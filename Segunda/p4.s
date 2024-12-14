6037780b52eaa7d554228f2cf270fcc747f6c318
Anna Myllenne <myllennearaujo17@gmail.com>
2024-12-14 10:50:22 -0300

    Parte 2 - 1. (7374 * 657) + (13121517 * 256) + 4294979641

---
+++ Segunda/p4.s
@@ -0,0 +1,24 @@
+  #
+  # modelo de saida para o compilador
+  #
+
+  .section .text
+  .globl _start
+
+_start:
+  MOV $7374, %rax
+  MOV $657, %rbx
+  MUL %rbx
+  MOV %rax, %rbx
+  MOV $13121517, %rax
+  MOV $256, %rcx
+  MUL %rcx
+  ADD %rbx, %rax
+  MOV $4294979641, %rbx
+  ADD %rbx, %rax
+
+  call imprime_num
+  call sair
+
+  .include "runtime.s"
+  
