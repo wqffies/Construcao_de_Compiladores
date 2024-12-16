0006ec940e1163c33d25dae5d1f3352368d0e249
Anna Myllenne <myllennearaujo17@gmail.com>
2024-12-16 11:00:45 -0300

    Parte 1 - 3. (72 - 101) * 4 + (14 * 77)

--- Segunda/p3.s
+++ Segunda/p3.s
@@ -9,11 +9,11 @@ _start:
   MOV $72, %rax
   SUB $101, %rax
   MOV $4, %rbx
-  MUL %rbx
+  IMUL %rbx
   MOV %rax, %rbx
   MOV $14, %rax
   MOV $77, %rcx
-  MUL %rcx
+  IMUL %rcx
   ADD %rbx, %rax
 
 
